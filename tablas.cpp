94a13cc20ed42f55df8343679fe57016ada5ee31
santiagovaca <santybolo@hotmail.com>
2015-01-28 11:00:28 -0500

    Update tablas.cpp
    primera parte del ejercicio con menu de opciones

--- tablas.cpp
+++ tablas.cpp
@@ -1,76 +1,155 @@
 #include <iostream>
-#include <fstream>
 #include <conio>
+#include <math>
+#include <fstream>
 
-void tablaand()
-main()
-{
-	ofstream archivo("C:/BC5/ARCHIVOS/nuevoarchivo.txt");
-	bool p;
-   archivo <<"p     Not p  " << endl;
-   archivo<<"---    ----- " << endl;
-   archivo<< true<<"       " <<!true<< endl;
-   archivo<<false<<"       "<<!false<<endl;
-   archivo<<endl;
+//******************************************************************************
+//Función que muestra el menú
+//******************************************************************************
+int menu(){
+	clrscr();
+	int op;
+   cout<<"************************************************"<<endl;
+   cout<<"************************************************"<<endl;
+   cout<<"***ESCUELA SUPERIOR POLITECNICA DE CHIMBORAZO***"<<endl;
+   cout<<"********TRABAJO FINAL DE PROGRAMACION I*********"<<endl;
+   cout<<"********INTEGRANTES: SANTIAGO VACA  745*********"<<endl;
+   cout<<"********           : JOEL RODRIGUES 744*********"<<endl;
+   cout<<"********           : VALERIA PEREZ  748*********"<<endl;
+   cout<<"************************************************"<<endl;
+   cout<<"************************************************"<<endl;
 
-   //ofstream archivo1("C:/BC5/ARCHIVOS/nuevoarchivo1.text",ios::app);
-   archivo<<"p q       p And q"<< endl;
-   archivo<<"-----    ------"<<endl;
-   archivo<< false<<""<<false<<"   	   " <<(false&&false) << endl;
-   archivo<< false<<""<<true<<"   	   " <<(false&&true) << endl;
-   archivo<< true<<""<<false<<"   	   " <<(true&&false) << endl;
-   archivo<< true<<""<<true<<"    	   " <<(true&&true) << endl;
-   archivo<< endl;
-   //archivo1.close();
-
-   //ofstream archivo2("C:/BC5/ARCHIVOS/nuevoarchivo2.text1",ios::app);
-	archivo<<"p q      p Or q"<< endl;
-   archivo<<"-----    -------"<<endl;
-   archivo<< false<<""<<false<<"  	  " <<(false||false) << endl;
-   archivo<< false<<""<<true<<"  	  " <<(false||true) << endl;
-   archivo<< true<<""<<false<<"  	  " <<(true||false) << endl;
-   archivo<< true<<""<<true<<"  	  " <<(true||true) << endl;
-   archivo<< endl;
-   //archivo2.close();
-
-   //ofstream archivo3("C:/BC5/ARCHIVOS/nuevoarchivo3.text1",ios::app);
-   archivo<<"p q 	    p No-And q"<< endl;
-   archivo<<"----- 	 -------"<<endl;
-   archivo<< false<<""<<false<<"  	   "; cout<<"1" << endl;
-   archivo<< false<<""<<true<<"   	   " ;cout<<"1"<< endl;
-   archivo<< true<<""<<false<<"   	   " ;cout<<"1"<< endl;
-   archivo<< true<<""<<true<<"   	   "; cout<<"0"<< endl;
-   archivo<< endl;
-   //archivo3.close();
-
-   //ofstream archivo4("C:/BC5/ARCHIVOS/nuevoarchivo4.text1",ios::app);
-   archivo<<"p q 	    p No-Or q"<< endl;
-   archivo<<"----- 	 -------"<<endl;
-   archivo<< false<<""<<false<<"  	   "; cout<<"1" << endl;
-   archivo<< false<<""<<true<<"   	   " ;cout<<"0"<< endl;
-   archivo<< true<<""<<false<<"   	   " ;cout<<"0"<< endl;
-   archivo<< true<<""<<true<<"   	   "; cout<<"0"<< endl;
-   archivo<< endl;
-   //archivo4.close();
-
-   //ofstream archivo5("C:/BC5/ARCHIVOS/nuevoarchivo5.text1",ios::app);
-   archivo<<"p q 	     p X-Or q"<< endl;
-   archivo<<"----- 	 -------"<<endl;
-   archivo<< false<<""<<false<<"  	   "; cout<<"0" << endl;
-   archivo<< false<<""<<true<<"   	   " ;cout<<"1"<< endl;
-   archivo<< true<<""<<false<<"   	   " ;cout<<"1"<< endl;
-   archivo<< true<<""<<true<<"   	   "; cout<<"0"<< endl;
-   archivo<< endl;
-   //archivo5.close();
-
-   //ofstream archivo6("C:/BC5/ARCHIVOS/nuevoarchivo6.text1",ios::app);
-   archivo<<"p q 	    p X-Nor q"<< endl;
-   archivo<<"----- 	 -------"<<endl;
-   archivo<< false<<""<<false<<"  	   "; cout<<"1" << endl;
-   archivo<< false<<""<<true<<"   	   " ;cout<<"0"<< endl;
-   archivo<< true<<""<<false<<"   	   " ;cout<<"0"<< endl;
-   archivo<< true<<""<<true<<"   	   "; cout<<"1"<< endl;
-   archivo<< endl;
-   archivo.close();
-   getch();
+   cout<<"            ***MENU DE OPCIONES***              "<<endl;
+   cout<<"1. Generar tablas de verdad con dos entradas"<<endl;
+   cout<<"2. Ver tablas de verdad con dos entradas"<<endl;
+   cout<<"3. Generar y ver tabla con n entradas"<<endl;
+   cout<<"4. Salir"<<endl;
+	cout<<"   Opcion: ";cin>>op;
+   return op;
 }
+
+//******************************************************************************
+//Función que genera tablas de verdad con 2 entradas
+//******************************************************************************
+void generarTablas2Entradas(){
+	clrscr();
+	cout<<"GENERAR TABLAS CON DOS ENTRADAS";
+}
+
+//******************************************************************************
+//Función que permite ver tablas de verdad con 2 entradas
+//******************************************************************************
+void verTablas2Entradas(){
+	clrscr();
+	cout<<"VER TABLAS CON DOS ENTRADAS";
+}
+
+
+//******************************************************************************
+//Función que genera y permite ver tablas de verdad con n entradas
+//******************************************************************************
+void generarTablasNEntradas(int nEntradas,int op){
+   char cadena[128];
+   int lineas = pow(2,nEntradas);
+   ofstream fs("nombre.txt");
+
+   if(op==1){
+//Genera el encabezado
+   for(int i=1;i<=nEntradas;i++){
+   	fs<<"X"<<i<<" ";
+      if(i==nEntradas)
+      	fs<<"AND"<<endl;
+   }
+//Genera la tabla de verdad AND
+   for(int i=1;i<=lineas;i++){
+   	bool and=1;
+      for(int j=nEntradas;j>=1;j--){
+      	int divisor=pow(2,j);
+         int digito;
+         digito=(i%divisor);
+
+         if(digito<=(divisor/2)){
+            if(digito==0)
+            	digito=1;
+            else
+         		digito=0;
+         }
+         else
+         	digito=1;
+
+         if(digito==0)
+         	and=0;
+         fs<<digito<<"  ";
+      }
+      fs<<and<<endl;
+   }
+
+   fs.close();
+   }
+   ifstream fe("nombre.txt");
+
+   while(!fe.eof()) {
+      fe.getline(cadena, 128);
+      cout << cadena << endl;
+   }
+   fe.close();
+}
+
+void verGenerarTablasNEntradas(){
+   clrscr();
+   cout<<"VER Y GENERAR TABLAS CON N ENTRADAS: "<<endl;
+   int entradas;
+   int op;
+   do{
+   	cout<<"Seleccione la tabla de verdad que desea generar y ver: "<<endl;
+      cout<<"1. AND"<<endl;
+      cout<<"2. OR"<<endl;
+      cout<<"3. NOT"<<endl;
+      cout<<"4. XOR"<<endl;
+      cout<<"5. NOR"<<endl;
+      cout<<"6. NAND"<<endl;
+      cout<<"7. X-NOR"<<endl;
+   	cout<<"   Opcion: ";cin>>op;
+   }while((op>7)||(op<1));
+
+   do{
+		cout<<"Ingrese cuantas entradas desea: ";cin>>entradas;
+   }while ((entradas<2)||(entradas>6));
+
+   generarTablasNEntradas(entradas,op);
+}
+
+//******************************************************************************
+//Función que realiza una acción según un valor ingresado
+//******************************************************************************
+void realizarAccion(int op){
+	switch(op){
+   	case 1:
+      	generarTablas2Entradas();
+         getch();
+         break;
+
+      case 2:
+      	verTablas2Entradas();
+         getch();
+         break;
+
+      case 3:
+      	verGenerarTablasNEntradas();
+         getch();
+         break;
+   }
+}
+
+//******************************************************************************
+//Función principal
+//******************************************************************************
+void main(){
+   int opcion=0;
+	while (opcion!=4){
+		opcion=menu();
+   	realizarAccion(opcion);
+   }
+}
+   
+  
